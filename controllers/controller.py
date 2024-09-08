3c248895ba14805d2791ddcc16460a1bfeb4007e
ashwanibakshi <ab324002@gmail.com>
2024-09-08 20:48:50 +0530

    added controller folder

---
+++ controllers/controller.py
@@ -0,0 +1,194 @@
+import math
+import json
+from flask_bcrypt import Bcrypt
+from flask import request
+from flask_mail import Message
+from slugify import slugify
+from models.dbModels import Article, Contact, Users,db
+from flask_login import LoginManager,login_required, login_user,current_user, logout_user
+
+
+with open('.\config\config.json','r') as c:
+    params = json.load(c) ['params']
+
+
+
+loginManger = LoginManager()
+bcrypt = Bcrypt()
+
+def getLoginManager():
+   return loginManger
+
+@loginManger.user_loader
+def load_user(user_id):
+    return Users.query.filter_by(id=int(user_id)).first()
+
+
+
+def getData():
+     articles = Article.query.filter().all()
+    
+     page = request.args.get('page')
+     last = math.ceil(len(articles)/int(params['no_of_posts']))
+
+     if (not str(page).isnumeric()):
+      page = 1
+      
+     page = int(page)  
+     articles = articles[(page-1)*int(params['no_of_posts']):(page-1)*int(params['no_of_posts'])+int(params['no_of_posts'])]
+     if page==1:
+      prev = "#"
+      next = "/?page="+ str(page+1)
+     elif page==last:
+      prev = "/?page="+ str(page-1)
+      next = "#"
+     else:
+      prev = "/?page="+ str(page-1)
+      next = "/?page="+ str(page+1)
+     data ={
+        "page" : "home.html",
+       "params" : "Home",
+       "posts" : articles ,
+       "prev"  : prev ,
+       "next"  : next
+     } 
+        
+     return data
+
+def slug(slugg):
+    article = Article.query.filter_by(slug=slugg).join(Users,Article.authorId==Users.id).add_columns(Users.name,Article.id,Article.title,Article.slug,Article.content,Article.date).first()  
+    # 'blog.html',params=slugg,post=article
+    data={
+      "page"  : 'blog.html',
+      "params": slugg,
+      "post"  : article
+    } 
+    return data
+
+
+def contacts(request,mail):
+    if (request.method=="POST"):
+       contact = Contact(
+       name     = request.form.get("name"),
+       email    = request.form.get("email"),
+       msg      = request.form.get("msg"),
+       location = request.form.get("location"),
+       subject  = request.form.get("subject"),
+       phno     = request.form.get("phno")
+       )
+    #    entry = Contact(name=namee,email=emaill,msg=message,location=locationn,subject=sub,phno=number)
+       db.session.add(contact)
+       db.session.commit()
+       msg= Message(
+            subject="Message from the blog",
+            sender= (contact.name,contact.email),
+            recipients=[params["email_user"]],
+            body = contact.location+'\n'+contact.subject+'\n'+contact.phno+'\n'+contact.msg 
+       )
+       mail.send(msg)
+    
+    data={
+       "page":"contact.html",
+       "params":"Contact Us"
+    }
+    return data
+
+def loginn(request):
+    if(request.method=="POST"):
+        users = Users.query.filter_by(email=request.form.get("email")).first() 
+        if (users is not None):
+            pwd  = request.form.get("password")
+            if(bcrypt.check_password_hash(users.password,pwd)):
+                if(request.form.get('remember') is not None):
+                   login_user(users,True)
+                else:   
+                    login_user(users,False) 
+                    data={
+                       "pass":True,
+                       "page":"dashboard"
+                    }  
+                return data
+            else:
+              data={
+                 "pass":False,
+                 "msg1":'Wrong Email Password',
+                 "msg2":'danger'
+               }
+            return data
+        else:
+            data={
+               "pass":False,
+                 "msg1":'User Is Not Registered',
+                 "msg2":'danger'
+               }
+        return data
+    else:
+       data={
+          "pass":"False",
+          "msg1" :""
+       }
+       return data
+    
+
+def registerr(request):
+   print(request.method)
+   if(request.method=="POST"):
+      users = Users.query.filter_by(email=request.form.get('email')).first()
+      if (users is not None): 
+         data={
+            "pass":False,
+            "msg1":'User Already Registered',
+            "msg2":'danger'
+         } 
+         return data
+      else:
+       user = Users(
+          name     = request.form.get("name"),
+          email    = request.form.get("email"),
+          password = bcrypt.generate_password_hash(request.form.get("password"))
+       )
+       db.session.add(user)
+       db.session.commit() 
+
+       data={
+          "pass":True,
+          "page":'login'
+       }
+       return data
+   else:
+       data={
+         "pass":False,
+         "msg1":"",
+         "msg2":""
+        } 
+       return data  
+   
+
+def dashboard():
+      dashData = Article.query.filter_by(authorId=current_user.id).all()
+      data={
+         "page"  :'/dashboard/dashboard.html',
+         "blogs" :dashData,
+         "params":"DashBoard",
+         "aid"   :current_user.id
+      }
+      return data
+
+def addPost(request):
+   if(request.method=="POST"):
+        article = Article(
+           title    = request.form.get("title"),
+           slug     = slugify(request.form.get("slug")),
+           content  = request.form.get("content"),
+           authorId = request.form.get("authorId")
+        )
+        db.session.add(article)
+        db.session.commit()
+   data={
+   "page"    :'/dashboard/addpost.html',
+   "author"  :current_user.name,
+   "authorid":current_user.id,
+   "params"  :"Add Post",
+   "aid"     : current_user.id
+   }
+   return data
--- index.py
+++ index.py
@@ -2,11 +2,12 @@ import math
 from flask import Flask,request,render_template,redirect,flash,session, url_for
 from flask_mail import Mail,Message
 from flask_bcrypt import Bcrypt
-from flask_login import LoginManager,login_required, login_user,current_user, logout_user
+from flask_login import login_required, login_user,current_user, logout_user
 import json
 
 from slugify import slugify
-from models.dbModels import db,Article,Contact,Users
+from controllers.controller import addPost, dashboard, getData, registerr,slug,contacts,loginn,getLoginManager
+from models.dbModels import Article,Contact,Users,db
 
 
 
@@ -16,7 +17,7 @@ with open('.\config\config.json','r') as c:
 app = Flask(__name__)
 bcrypt = Bcrypt(app)
 
-login_manger = LoginManager()
+login_manger = getLoginManager()
 login_manger.login_view='login'
 login_manger.init_app(app)
 
@@ -34,116 +35,56 @@ app.config.update(
 )
 mail = Mail(app)
 
-@login_manger.user_loader
-def load_user(user_id):
-    return Users.query.filter_by(id=int(user_id)).first()
+
+# @login_manger.user_loader
+# def load_user(user_id):
+#     return Users.query.filter_by(id=int(user_id)).first()
 
 @app.route('/',methods=["GET"])
 def home():
-    articles = Article.query.filter().all()
-    
-    page = request.args.get('page')
-    last = math.ceil(len(articles)/int(params['no_of_posts']))
-
-    if (not str(page).isnumeric()):
-      page = 1
-      
-    page = int(page)  
-    articles = articles[(page-1)*int(params['no_of_posts']):(page-1)*int(params['no_of_posts'])+int(params['no_of_posts'])]
-    if page==1:
-     prev = "#"
-     next = "/?page="+ str(page+1)
-    elif page==last:
-     prev = "/?page="+ str(page-1)
-     next = "#"
-    else:
-     prev = "/?page="+ str(page-1)
-     next = "/?page="+ str(page+1)
-    return render_template('home.html',params="Home",posts=articles,prev=prev,next=next)
+    data2 = getData() 
+    return render_template(data2["page"],params = data2["params"],posts=data2["posts"],prev=data2["prev"],next=data2["next"])
 
 @app.route('/blog/<slugg>',methods=["GET"])
 def blog(slugg):
-    article = Article.query.filter_by(slug=slugg).join(Users,Article.authorId==Users.id).add_columns(Users.name,Article.id,Article.title,Article.slug,Article.content,Article.date).first()  
-    print(article)
-    return render_template('blog.html',params=slugg,post=article)
+    data = slug(slugg)
+    print('123',data)
+    return render_template(data["page"],params=data["params"],post=data["post"])
 
 @app.route('/contact',methods=["GET","POST"])
 def contact():
-    if (request.method=="POST"):
-       contact = Contact(
-       name     = request.form.get("name"),
-       email    = request.form.get("email"),
-       msg      = request.form.get("msg"),
-       location = request.form.get("location"),
-       subject  = request.form.get("subject"),
-       phno     = request.form.get("phno")
-       )
-    #    entry = Contact(name=namee,email=emaill,msg=message,location=locationn,subject=sub,phno=number)
-       db.session.add(contact)
-       db.session.commit()
-       msg= Message(
-            subject="Message from the blog",
-            sender= (contact.name,contact.email),
-            recipients=[params["email_user"]],
-            body = contact.location+'\n'+contact.subject+'\n'+contact.phno+'\n'+contact.msg 
-       )
-       mail.send(msg)
-    return render_template('contact.html',params="Contact Us") 
+    data = contacts(request,mail)
+    return render_template(data["page"],params=data["params"]) 
 
 @app.route('/login',methods=["GET","POST"])
 def login():
-    if(request.method=="POST"):
-        users = Users.query.filter_by(email=request.form.get("email")).first() 
-        if (users is not None):
-            pwd  = request.form.get("password")
-            if(bcrypt.check_password_hash(users.password,pwd)):
-                if(request.form.get('remember') is not None):
-                   login_user(users,True)
-                else:   
-                    login_user(users,False)   
-                return redirect('dashboard')
-            else:
-              flash('Wrong Email Password','danger')  
-        else:
-           flash('User Is Not Registered','danger')  
+    data = loginn(request)
+    if(data["pass"]==True):
+        return redirect(data["page"])
+    if(data["msg1"]):
+     flash(data["msg1"],data["msg2"])
     return render_template('login.html')
 
 @app.route('/register',methods=["GET","POST"])
 def register():
-    if(request.method=="POST"):
-      users = Users.query.filter_by(email=request.form.get('email')).first()
-      if (users is not None): 
-       flash('User Already Registered','danger')  
-      else:
-       user = Users(
-          name     = request.form.get("name"),
-          email    = request.form.get("email"),
-          password = bcrypt.generate_password_hash(request.form.get("password"))
-       )
-       db.session.add(user)
-       db.session.commit() 
-    return render_template('register.html')
+    data = registerr(request)
+    if(data["pass"]==True):
+        return redirect(data["page"])
+    else:
+     flash(data["msg1"],data["msg2"]) 
+     return render_template('register.html')
 
 @app.route('/dashboard',methods=["GET"])
 @login_required
 def dash():
-    dashData = Article.query.filter_by(authorId=current_user.id).all()
-    print(dashData)
-    return render_template('/dashboard/dashboard.html',blogs=dashData,params="DashBoard",aid=current_user.id)
+    data = dashboard()
+    return render_template(data["page"],blogs=data["blogs"],params=data["params"],aid=data["aid"])
 
 @app.route('/addpost',methods=["GET","POST"])
 @login_required
 def addpost():
-    if(request.method=="POST"):
-        article = Article(
-           title    = request.form.get("title"),
-           slug     = slugify(request.form.get("slug")),
-           content  = request.form.get("content"),
-           authorId = request.form.get("authorId")
-        )
-        db.session.add(article)
-        db.session.commit()
-    return render_template('/dashboard/addpost.html',author=current_user.name, authorId=current_user.id,params="Add Post",aid=current_user.id)
+    data = addPost(request)
+    return render_template(data["page"],author=data["author"], authorId=data["authorid"],params=data["params"],aid=data["aid"])
 
 
 @app.route('/editpost',methods=["POST"])
